35e0a3b2ced2f2f2a4cc397f4e6d7f3169e86d6f
kristof de spiegeleer <kristof@incubaid.com>
2017-04-17 13:27:48 +0400

    cmds linked

--- JumpScale9/core/InstallTools.py
+++ JumpScale9/core/InstallTools.py
@@ -2271,7 +2271,7 @@ class InstallTools(GitMethods, FSMethods, ExecutorMethods, SSHMethods):
         cmdsDir = j.sal.fs.joinPaths(js9_codedir, "cmds")
 
         for item in j.sal.fs.listFilesInDir(cmdsDir):
-            j.sal.fs.symlink(item, "/opt/local/bin/%s" % j.sal.fs.getBaseName(item), overwriteTarget=True)
+            j.sal.fs.symlink(item, "/usr/local/bin/%s" % j.sal.fs.getBaseName(item), overwriteTarget=True)
 
     def fixCodeChangeDirVars(self, branch="8.2.0"):
         """
