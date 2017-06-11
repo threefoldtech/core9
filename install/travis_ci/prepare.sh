5286c57aeeba25ad1da59cfee435fdcf9a2c873a
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-11 14:10:20 +0200

    updates

--- install/travis_ci/prepare.sh
+++ install/travis_ci/prepare.sh
@@ -4,7 +4,9 @@
 ssh-keygen -t rsa -N "" -f ~/.ssh/main
 export SSHKEYNAME=main
 
-export GIGBRANCH=$(git branch | grep \* | cut -d ' ' -f2-)
+export GIGSAFE=1
+export GIGBRANCH=$(git symbolic-ref --short HEAD)
+export GIGBRANCH=travis_ci
 echo $GIGBRANCH
 curl https://raw.githubusercontent.com/Jumpscale/developer/master/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
 
