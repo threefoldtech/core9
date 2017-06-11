66db3d5d40642747ba85c78e40899ebfa347600b
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-11 13:35:07 +0200

    generate ssh-key and export the env variable

--- install/travis_ci/prepare.sh
+++ install/travis_ci/prepare.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# generate ssh keys
+ssh-keygen -t rsa -N "" -f ~/.ssh/main
+export SSHKEYNAME=main
+
 export GIGBRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo $GIGBRANCH
 curl https://raw.githubusercontent.com/Jumpscale/developer/${GIGBRANCH}/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
 
 # build image
