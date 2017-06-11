1d134b000e845ed2e4e3feb8ca3a0e9f3f3a7a73
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-11 13:10:42 +0200

    initial travis CI configurations

---
+++ install/travis_ci/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GIGBRANCH=$(git rev-parse --abbrev-ref HEAD)
+curl https://raw.githubusercontent.com/Jumpscale/developer/${GIGBRANCH}/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
+js9_build
