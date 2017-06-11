c1c973e6d62471f503e12be5ccd49aa5559f836d
Abdelrahman Hussein <ahussein@codescalers.com>
2017-06-11 13:14:58 +0200

    make sure prepare file is executable

--- install/travis_ci/prepare.sh
+++ install/travis_ci/prepare.sh
@@ -2,4 +2,6 @@
 
 export GIGBRANCH=$(git rev-parse --abbrev-ref HEAD)
 curl https://raw.githubusercontent.com/Jumpscale/developer/${GIGBRANCH}/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
+
+# build image
 js9_build
