a7f123a6f8f14a5456d5554b7b898319d28e66cf
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-08-24 01:34:58 +0200

    update test scripts

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export SSHKEYNAME=main
-source ~/.jsenv.sh
-js9_start
-ssh -A -i ~/.ssh/main root@localhost -p 2222 'cd /root/gig/code/github/jumpscale/core9; /bin/bash test.sh'
+# export SSHKEYNAME=main
+# source ~/.jsenv.sh
+# js9_start
+# ssh -A -i ~/.ssh/main root@localhost -p 2222 'cd /root/gig/code/github/jumpscale/core9; /bin/bash test.sh'
+cd /opt/code/github/jumpscale/core9; sudo /bin/bash test.sh
