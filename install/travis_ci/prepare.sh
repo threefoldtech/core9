a0dd1db841163d4a00226e167f582ffea0b57f9a
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2017-08-23 18:09:03 +0200

    use base scripts to build

--- install/travis_ci/prepare.sh
+++ install/travis_ci/prepare.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # generate ssh keys
-ssh-keygen -t rsa -N "" -f ~/.ssh/main
-export SSHKEYNAME=main
+# ssh-keygen -t rsa -N "" -f ~/.ssh/main
+# export SSHKEYNAME=main
 
-export GIGSAFE=1
-export GIGDEVELOPERBRANCH=master
+# export GIGSAFE=1
+# export GIGDEVELOPERBRANCH=master
 
-curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
+# curl https://raw.githubusercontent.com/Jumpscale/developer/$GIGDEVELOPERBRANCH/jsinit.sh?$RANDOM > /tmp/jsinit.sh; bash /tmp/jsinit.sh
 
 # build image
-source ~/.jsenv.sh
-js9_build
+# source ~/.jsenv.sh
+# js9_build
+
+export ZUTILSBRANCH=${ZUTILSBRANCH:-fixes}
+
+curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;bash /tmp/install.sh
