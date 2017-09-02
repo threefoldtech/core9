990be74b888924d791fd08280bbbd17dd89efa0a
Abdelrahman Hussein <ahussein@codescalers.com>
2017-09-02 16:23:48 +0200

    fixes for docker installer from bash repo

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,8 @@
 eval $(ssh-agent)
 ssh-add
 
+# Start js9 container
+sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZDockerActive -b jumpscale/js9_full -i js9_full"
+
 # Run tests
 sudo -HE bash -c "ssh -tA  root@localhost -p 2222 \"cd /opt/code/github/jumpscale/core9; /bin/bash test.sh\""
