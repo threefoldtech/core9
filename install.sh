05d38eab9707161001259baddac93193ab5e7077
someid <kristof@incubaid.com>
2017-09-27 08:08:02 +0200

    installer

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 echo "INSTALL BASHTOOLS"
 curl https://raw.githubusercontent.com/Jumpscale/bash/master/install.sh?$RANDOM > /tmp/install.sh;bash /tmp/install.sh
 
+echo "load zlibs"
+source ~/code/github/jumpscale/bash/zlibs.sh 2>&1 > /dev/null
+source /opt/code/github/jumpscale/bash/zlibs.sh 2>&1 > /dev/null
+
+echo "install js9"
 ZInstall_host_js9
 
 pip3 install Cython
