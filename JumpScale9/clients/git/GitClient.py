2b4c71002f9a9c15108021c7c13bf09ca667971e
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2017-11-14 13:37:41 +0200

    Adds fix getBranchOrTag (#109)
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- JumpScale9/clients/git/GitClient.py
+++ JumpScale9/clients/git/GitClient.py
@@ -85,9 +85,9 @@ class GitClient:
 
     def getBranchOrTag(self):
         try:
-            return 'tag', self.repo.git.describe('--tags')
+            return 'tag', self.repo.git.describe('--tags', '--exact-match')
         except BaseException:
-            return 'branch', self.branchName
+            return 'branch', self.repo.head.ref.name
 
     def switchBranch(self, branchName, create=True):  # NOQA
         if create:
@@ -209,7 +209,7 @@ class GitClient:
             return
         return self.repo.index.commit(message)
 
-    def push(self, force=False):    
+    def push(self, force=False):
         if force:
             self.repo.git.push('-f')
         else:
