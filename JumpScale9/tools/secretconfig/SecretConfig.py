84259c9733bd847823c18e21e7916b4fe6c2571f
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-12-22 13:22:51 +0200

    SecretConfig module improvement
    - add list, set, delete method on the factory
    - add documentation
    - add doctring
    - rename some class to something more meaningfull

--- JumpScale9/tools/secretconfig/SecretConfig.py
+++ JumpScale9/tools/secretconfig/SecretConfig.py
@@ -1,4 +1,5 @@
 from js9 import j
+import os
 
 TEMPLATE = """
 fullname = ""
@@ -7,9 +8,31 @@ login_name = ""
 ssh_key_name = ""
 """
 
-BaseConfig=j.tools.formbuilder.baseclass_get()
+FormBuilderBase = j.tools.formbuilder.baseclass_get()
+
 
 class JSBaseClassSecret():
+
+    def __init__(self):
+        self.__jslocation__ = None
+        self.instance = None
+        self._TEMPLATE = TEMPLATE
+
+    @property
+    def _location(self):
+        # make sure __jslocation__ always exists and raise if not
+        # cause we can set a default value
+        if not hasattr(self, '__jslocation__') or \
+                self.__jslocation__ is None or self.__jslocation__ == '':
+            raise RuntimeError("__jslocation__ attribute doesn't exist or is not set.")
+        return self.__jslocation__
+
+    @property
+    def _instance(self):
+        # make sure self.instance always exists and set a default value
+        if not hasattr(self, 'instance') or self.instance is None or self.instance == '':
+            self.instance = 'main'
+        return self.instance
 
     @property
     def _configure_class(self):
@@ -17,43 +40,49 @@ class JSBaseClassSecret():
 
     @property
     def _configure_template(self):
-        return self._TEMPLATE     
+        if not hasattr(self, '_TEMPLATE') or \
+                self._TEMPLATE is None or self._TEMPLATE == '':
+            raise RuntimeError("self._TEMPLATE attribute doesn't exist or is not set")
+        return self._TEMPLATE
 
     @property
     def config(self):
-        return j.tools.secretconfig.get(location=self.__jslocation__,instance=self.instance)
+        return j.tools.secretconfig.get(location=self._location, instance=self._instance)
 
     @config.setter
-    def config(self,val):
-        if j.data.types.dict.check(val)==False:
-            raise RuntimeError("need to be dict")
-        return j.tools.secretconfig.configure(location=self.__jslocation__,instance=self.instance,updatedict=val,configure=False)
-    
-    @property
-    def _path(self):
-        return j.sal.fs.getDirName(os.path.abspath(__file__)).rstrip("/")    
+    def config(self, val):
+        if j.data.types.dict.check(val) is False:
+            raise TypeError("need to be dict")
+        return j.tools.secretconfig.config_update(location=self._location, instance=self._instance, updatedict=val)
 
-class MyConfig(BaseConfig):
 
-    def init(self):
-        self.auto_disable.append("ssh_key_name") #makes sure that this property is not auto populated, not needed when in form_add_items_pre
+class MyConfig(FormBuilderBase):
+    """
+    This class let the user tune the form displayed during configuration.
+    By default configure only show the inputs from the template,
+    this class allow to enhance the form with custom inputs.
+    """
+
+    def __init__(self):
+        super().__init__()
+        # makes sure that this property is not auto populated, not needed when in form_add_items_pre
+        self.auto_disable.append("ssh_key_name")
 
     def form_add_items_post(self):
-        #SSHKEYS
+        # SSHKEYS
         sshpath = "%s/.ssh" % (j.dirs.HOMEDIR)
-        # keynames = [j.sal.fs.getBaseName(item) for item in j.clients.ssh.ssh_keys_list_from_agent()] #load all ssh keys loaded in mem
         keynames = [j.sal.fs.getBaseName(item)[:-4] for item in j.sal.fs.listFilesInDir(sshpath, filter="*.pub")]
-        if len(keynames)==0:
-            raise RuntimeError("load ssh-agent")
-        self.widget_add_multichoice("ssh_key_name",keynames)
-
+        if len(keynames) == 0:
+            raise RuntimeError("No ssh key found in ssh-agent. Make sure ssh-agent is running and at least one key is loaded")
+        self.widget_add_multichoice("ssh_key_name", keynames)
 
 
 class SecretConfigFactory(JSBaseClassSecret):
 
     def __init__(self):
+        super().__init__()
         self.__jslocation__ = "j.tools.secretconfig"
-        self._path=""
+        self._path = ""
         self._TEMPLATE = TEMPLATE
 
     @property
@@ -62,13 +91,13 @@ class SecretConfigFactory(JSBaseClassSecret):
 
     @property
     def path_configrepo(self):
-        if self._path=="":
-            res=j.clients.git.getGitReposListLocal()
-            for key,path in res.items():
-                checkpath="%s/.jsconfig"%path
+        if self._path == "":
+            res = j.clients.git.getGitReposListLocal()
+            for key, path in res.items():
+                checkpath = "%s/.jsconfig" % path
                 if key.startswith("config_"):
                     if j.sal.fs.exists(checkpath):
-                        self._path=path
+                        self._path = path
                         return self._path
             raise RuntimeError("Cannot find path for configuration repo, please checkout right git repo & run 'js9_secret init' in that repo ")
         return self._path
@@ -79,147 +108,192 @@ class SecretConfigFactory(JSBaseClassSecret):
 
     def configure(self, location="myconfig", instance="main"):
         """
-        call the configurator, which will allow arguments to be filled in
+        Will display a npyscreen form to edit the configuration
         """
-        if location=="myconfig":
-            jsclient_object=self
+        if location == "myconfig":
+            jsclient_object = self
         else:
-            jsclient_object=eval(location)
+            jsclient_object = eval(location)
 
-        sc=SecretConfig(instance=instance,jsclient_object=jsclient_object)
+        sc = SecretConfig(instance=instance, jsclient_object=jsclient_object)
         sc.configure()
         sc.save()
         return sc
 
-    def config_update(self, location="myconfig", instance="main",updatedict={}):
+    def config_update(self, location="myconfig", instance="main", updatedict={}):
         """
-        call the configurator, which will allow arguments to be filled in
+        update the configuration by giving a dictionnary. The configuration will
+        be updated with the value of updatedict
         """
-        if location=="myconfig":
-            jsclient_object=self
+        if location == "myconfig":
+            jsclient_object = self
         else:
-            jsclient_object=eval(location)
+            jsclient_object = eval(location)
 
-        sc=SecretConfig(instance=instance,jsclient_object=jsclient_object)
+        sc = SecretConfig(instance=instance, jsclient_object=jsclient_object)
         sc.data = updatedict
         sc.save()
         return sc
 
     def get(self, location="myconfig", instance="main"):
         """
+        return a secret config
         """
-        if location=="myconfig":
-            jsclient_object=self
+        if location == "myconfig":
+            jsclient_object = self
         else:
-            jsclient_object=eval(location)
+            jsclient_object = eval(location)
 
-        sc=SecretConfig(instance=instance,jsclient_object=jsclient_object)
-        return sc    
+        sc = SecretConfig(instance=instance, jsclient_object=jsclient_object)
+        return sc
+
+    def set(self, location, instance, config=None):
+        """
+        create a new config
+
+        @param location: location of the client
+        @param instance: instance name
+        @param config: optional configuration to set.
+        @type config: dict
+        """
+        # create the config directory and file, so we don't trigger the form
+        # when creating a SercretConfig object
+        path = j.sal.fs.joinPaths(j.tools.secretconfig.path_configrepo, location, instance + '.toml')
+        j.sal.fs.createDir(j.sal.fs.getParent(path))
+        j.sal.fs.writeFile(path, "")
+
+        jsclient_object = eval(location)
+
+        sc = SecretConfig(instance=instance, jsclient_object=jsclient_object)
+        if config is not None:
+            sc.data = config
+        sc.save()
+        return sc
+
+    def list(self, location):
+        """
+        list all the existing instance name for a location
+
+        @return: list of instance name
+        """
+        instances = []
+
+        root = j.sal.fs.joinPaths(self.path_configrepo, location)
+        if not j.sal.fs.exists(root):
+            return instances
+
+        jsclient_object = eval(location)
+
+        for cfg_path in j.sal.fs.listFilesInDir(root):
+            cfg_name = j.sal.fs.getBaseName(cfg_path)
+            if cfg_name in ('.git', '.jsconfig'):
+                continue
+            # trim the extension
+            instance_name = cfg_name.split(os.path.extsep)[0]
+            instances.append(instance_name)
+        return instances
+
+    def delete(self, location, instance):
+        path = j.sal.fs.joinPaths(j.tools.secretconfig.path_configrepo, self.__jslocation__, instance + '.toml')
+        if not j.sal.fs.exists(path):
+            return
+
+        j.sal.fs.remove(path)
 
     def init(self):
-        curdir=j.sal.fs.getcwd()
-        gitdir="%s/.git"%curdir
-        if not j.sal.fs.exists(gitdir):
+        curdir = j.sal.fs.getcwd()
+        gitdir = "%s/.git" % curdir
+        if not j.sal.fs.exists(gitdir) or not j.sal.fs.isDir(gitdir):
             raise RuntimeError("am not in root of git dir")
-        self.path=curdir
+        self._path = curdir
         j.sal.fs.touch(".jsconfig")
         self.configure()
 
-        
 
 class SecretConfig():
 
-    def __init__(self,instance="main",jsclient_object=None,location=None):
+    def __init__(self, instance="main", jsclient_object=None, location=None):
         """
         jsclient_object is e.g. j.clients.packet.net
         """
-        if location==None:
-            self.location = jsclient_object.__jslocation__ 
+        if location is None:
+            self.location = jsclient_object.__jslocation__
         else:
             self.location = location
-        self.instance=instance
-        # self._category = ""
+        self.instance = instance
         self._template = {}
         self.jsclient_object = jsclient_object
 
-        dirpath = j.tools.secretconfig.path_configrepo+"/%s"%self.location
+        dirpath = j.tools.secretconfig.path_configrepo + "/%s" % self.location
 
         j.sal.fs.createDir(dirpath)
-        
-        self.path="%s/%s.toml"%(dirpath,instance)
-            
+
+        self.path = j.sal.fs.joinPaths(dirpath, instance + '.toml')
+
         if not j.sal.fs.exists(self.path):
-            self._data={}
+            self._data = {}
             self.configure()
             self.save()
         else:
-            content=j.sal.fs.fileGetContents(self.path)
-            config=j.data.serializer.toml.loads(content)
-            #merge found config into template
-            self._data,error=j.data.serializer.toml.merge(self.template,config,listunique=True)
-        
+            content = j.sal.fs.fileGetContents(self.path)
+            config = j.data.serializer.toml.loads(content)
+            # merge found config into template
+            self._data, error = j.data.serializer.toml.merge(self.template, config, listunique=True)
+
     def configure(self):
-        myconfig=self.jsclient_object._configure_class(name=self.path, config=self.data,template=self.template)
+        myconfig = self.jsclient_object._configure_class(name=self.path, config=self.data, template=self.template)
         myconfig.run()
-        self.data=myconfig.config
+        self.data = myconfig.config
 
     def save(self):
-        #at this point we have the config & can write (self._data has the encrypted pieces)
-        j.sal.fs.writeFile(self.path,j.data.serializer.toml.fancydumps(self._data))
-
-
-    # @property
-    # def category(self):
-    #     if self._category=="":
-    #         location=self.location.strip(".")        
-    #         if location.startswith("j."):
-    #             category = location[2:]
-    #         else:
-    #             category = location
-    #         category = category.replace(".","_")
-    #         self._category = category.lower().strip()
-    #     return self._category
+        # at this point we have the config & can write (self._data has the encrypted pieces)
+        j.sal.fs.writeFile(self.path, j.data.serializer.toml.fancydumps(self._data))
 
     @property
     def template(self):
-        if self._template=={}:            
-            self._template=j.data.serializer.toml.loads(self.jsclient_object._configure_template)
+        if self._template == {}:
+            self._template = j.data.serializer.toml.loads(self.jsclient_object._configure_template)
         return self._template
 
     @property
     def data(self):
-        res={}
-        for key,item in self._data.items():            
-            if key.endswith("_"):
-                ttype= j.data.types.type_detect(self.template[key])
-                if ttype.BASETYPE == "string":
-                    res[key]=j.data.nacl.default.decryptSymmetric(item,hex=True).decode()
+        res = {}
+        for key, item in self._data.items():
+            ttype = j.data.types.type_detect(self.template[key])
+            if key.endswith("_") and ttype.BASETYPE == "string":
+                # FIXME: this is buggy in the case the key ends with _ but is not a string, the value is lost
+                if item != '':
+                    res[key] = j.data.nacl.default.decryptSymmetric(item, hex=True).decode()
+                else:
+                    res[key] = ''
             else:
-                res[key]=item                
+                res[key] = item
         return res
 
+    @data.setter
+    def data(self, value):
+        if j.data.types.dict.check(value) is False:
+            raise TypeError("value needs to be dict")
+
+        for key, item in value.items():
+            if key not in self.template:
+                raise RuntimeError("Cannot find key:%s in template for %s" % (key, self))
+
+            ttype = j.data.types.type_detect(self.template[key])
+            if key.endswith("_") and ttype.BASETYPE == "string":
+                # FIXME: this is buggy in the case the key ends with _ but is not a string, the value is lost
+                if item != '':
+                    item = j.data.nacl.default.encryptSymmetric(item, hex=True, salt=item)
+            self._data[key] = item
+
     @property
-    def yaml(self):    
+    def yaml(self):
         return j.data.serializer.toml.fancydumps(self._data)
 
-    @data.setter
-    def data(self,val):
-        if j.data.types.dict.check(val)==False:
-            raise RuntimeError("need to be dict")
-        for key,item in val.items():
-            if key not in self.template:
-                raise RuntimeError("Cannot find key:%s in template for %s"%(key,self))
-            ttype= j.data.types.type_detect(self.template[key])
-            if key.endswith("_") and ttype.BASETYPE == "string":
-                item = j.data.nacl.default.encryptSymmetric(item,hex=True,salt=item)
-            self._data[key] = item
-                    
     def __str__(self):
-        out = "config:%s:%s\n\n"%(self.location, self.instance)    
+        out = "config:%s:%s\n\n" % (self.location, self.instance)
         out += self.yaml
         out += "===========================\n"
         return out
 
-    __repr__=__str__
-
-
+    __repr__ = __str__
--- JumpScale9/tools/secretconfig/readme.md
+++ JumpScale9/tools/secretconfig/readme.md
@@ -1,3 +1,57 @@
 
 # how to create a configuration object
 
+
+## How to enable a jumpscale module to have a secret config management
+
+- Make you module inherits from the secret config base class, available at `j.tools.secretconfig.base_class_secret`.
+- Make sure to call the constructor of the base class. (`super().__ini__()`)
+- Define the template of your configuration. This template is a multi string representing a dictionary.  
+You need to specified a default value for each key. The type of the value will be automatically deducted. The type per key will be remember and checked when updating the config. If you tried to set a value with a wrong type, an exception will be raised.  
+If the key last end with an '_' then the value will always be encrypted.
+
+- set the TEMPLATE attribute to the template you've defined
+
+- before accessing the `self.config` attribute, set `self.instance`.
+
+Example code:
+
+```python
+
+
+CONFIG_TEMPLATE = """
+host = ""
+port = 0
+secret_ = ""
+"""
+
+SecretConfigBase = j.tools.secretconfig.base_class_secret
+
+class MyModuleFactory(SecretConfigBase):
+
+    def __init__(self):
+        # you need to call the constructor
+        # of the base class to boostrap the config
+        super().__ini__()
+        # define the jslocation like any other js module
+        self.__jslocation__ = "j.client.mymodule"
+        # set self.TEMPLATE to your defined TEMPLATE
+        # self.TEMPLATE is given by SecretConfigBase
+        self.TEMPLATE = CONFIG_TEMPLATE
+    
+    def get(self, instance='main'):
+        """
+        get an instance of your module
+        """
+        # self.instance is give by SecretConfigBase
+        # you need to set it for self.config to return the proper values
+        self.instance = instance
+        host = self.config.data['host']
+        port = self.config.data['port']
+        # the secret will get decrypted on the fly
+        secret = self.config.data['secret']
+        # create an instance
+        i = Instance(host, port, secret)
+        return i
+    
+```
