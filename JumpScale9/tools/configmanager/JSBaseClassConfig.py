20ab3957b4507b8e8bd7e301e9f45e07e00a0f08
Reem Khamis <khamisr@codescalers.com>
2017-12-29 15:27:02 +0200

    Lazy load config for clients

--- JumpScale9/tools/configmanager/JSBaseClassConfig.py
+++ JumpScale9/tools/configmanager/JSBaseClassConfig.py
@@ -3,28 +3,26 @@ from js9 import j
 # import copy
 
 
-class JSBaseClassConfig():
+class JSBaseClassConfig:
 
-    def __init__(self, instance="main", data={}, parent=None,template=None,ui=None):
+    def __init__(self, instance="main", data={}, parent=None, template=None, ui=None):
         self._single_item = True
         s = self
         if parent is not None:
             s.__jslocation__ = parent.__jslocation__
-        self.logger = j.logger.get(self.__jslocation__)        
-        if ui==None:
-            self.ui = j.tools.formbuilder.baseclass_get() #is the default class
+        self.logger = j.logger.get(self.__jslocation__)
+        if ui is None:
+            self._ui = j.tools.formbuilder.baseclass_get()  # is the default class
         else:
-            self.ui=ui
-        if template==None:
-            raise RuntimeError("template needs to be specified, needs to be yaml or dict")
-        self._config = j.tools.configmanager._get_for_obj(self,instance=instance,data=data,template=template,ui=self.ui)
-
-        if self._config.load()>0:
-            self.interactive()
-        elif self.config_check() not in [None,"",0]:
-            self.interactive()
-
-
+            self._ui = ui
+        if template is None:
+            raise RuntimeError(
+                "template needs to be specified, needs to be yaml or dict")
+        self._config = None
+        self._instance = instance
+        self._data = data
+        self._parent = parent
+        self._template = template
 
     def reset(self):
         self.config.instance_set(self.instance)
@@ -32,7 +30,12 @@ class JSBaseClassConfig():
     @property
     def config(self):
         if self._config is None:
-            raise RuntimeError("could not find config obj on secret base class, make sure has been properly initialized check MyConfig.py in core for example.")
+            self._config = j.tools.configmanager._get_for_obj(
+                self, instance=self._instance, data=self._data, template=self._template, ui=self._ui)
+            if self._config.load() > 0:
+                self.interactive()
+            elif self.config_check() not in [None, "", 0]:
+                self.interactive()
         return self._config
 
     @config.setter
@@ -49,37 +52,37 @@ class JSBaseClassConfig():
 
     def interactive(self):
         if j.tools.configmanager.interactive:
-            print("Did not find config file:%s, will ask for initial configuration information."%self.config.location)
-            self.config.instance=j.tools.console.askString("specify name for instance", defaultparam=self.config.instance)
+            print("Did not find config file:%s, will ask for initial configuration information." % self.config.location)
+            self.config.instance = j.tools.console.askString("specify name for instance", defaultparam=self.config.instance)
             self.configure()
         else:
-            raise RuntimeError("configuration not found for :%s, please run 'js9_config configure -l %s "%(self.__jslocation__,self.__jslocation__))
-   
+            raise RuntimeError("configuration not found for :%s, please run 'js9_config configure -l %s " % (self.__jslocation__, self.__jslocation__))
+
     def configure(self):
         """
         call the form build to represent this object
         """
 
-        if self.ui is None:
+        if self._ui is None:
             raise RuntimeError("cannot call configure UI because not defined yet, is None")
-        myui = self.ui(name=self.config.path, config=self.config.data, template=self.config.template)
+        myui = self._ui(name=self.config.path, config=self.config.data, template=self.config.template)
 
         while True:
             myui.run()
-            self.config.data =  myui.config #config in the ui is a std dict
-            msg=self.config_check()
-            if msg!=None and msg!="":
+            self.config.data = myui.config  # config in the ui is a std dict
+            msg = self.config_check()
+            if msg is not None and msg != "":
                 print(msg)
                 j.tools.console.askString("please correct the information in next configuraton screen, press enter")
             else:
                 break
-        
+
         self.config.save()
 
         return self.config
 
     def config_check(self):
-        return 0
+        return None
 
     def __str__(self):
         out = "js9_object:"
