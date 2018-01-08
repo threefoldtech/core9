ad89c84197099009ad90cb4918ab20cdfe74d9c2
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2018-01-08 13:18:29 +0200

    fix

--- tests/jumpscale_test/test02_j_state.py
+++ tests/jumpscale_test/test02_j_state.py
@@ -21,7 +21,7 @@ class TestJSTATE(TestcasesBase):
         content.update(data)
         with open(cls.config_path, 'w') as f:
             pytoml.dump(content, f)
-
+        return content
 
     @classmethod
     def setUpClass(cls):
@@ -43,60 +43,84 @@ class TestJSTATE(TestcasesBase):
                 'd_key_3':False
             }
         }
-        self.update_config(test_config)
+        self.client._configJS = self.update_config(test_config)
+        self.client.configSave()
 
     def tearDown(self):
         self.reset_config()
 
-    def test001_config(self):
-        js_config = self.client.config
-        self.assertDictEqual(js_config, self.config_file_content)
-
-    def test002_config_get(self):
+    def test001_config_get(self):
         # self.lg('Get value of an existing key')
         value = self.client.configGet('key_1')
         self.assertEqual(value, 'value_1')
 
-        # self.lg('Get value of a non existing key')
+        # self.lg('Get the value of a non-existing key')
         with self.assertRaises(j.exceptions.Input) as e:
             self.client.configGet('new_key')
         
-        # self.lg('Get value of a non existing key with default value and set is false')
+        # self.lg('Get the value of a non-existing key with default value and set is false')
         value = self.client.configGet('new_key', defval='new_value')
         self.assertEqual(value, 'new_value')
-        self.assertFalse(self.client.config.get('fake_key'))
 
-        # self.lg('Get value of non existing key with default value and set is true')
+        with self.assertRaises(j.exceptions.Input) as e:
+            self.client.configGet('new_value')
+
+        # self.lg('Get the value of non existing key with default value and set is true')
         value = self.client.configGet('new_key', defval='new_value', set=True)
         self.assertEqual(value, 'new_value')
-        self.assertEqual(self.client.config.get('new_key'), 'new_value')
-
+        self.assertEqual(self.client.configGet('new_key'), 'new_value')
     
-    def test003_config_get_for_dict(self):
+    def test002_config_get_form_dict(self):
+        # self.lg('Get the value of an existing key from existing dict')
+        value = self.client.configGetFromDict('dict_1', 'd_key_1')
+        self.assertEqual(value, 'd_value_1')
+
+        # self.lg('Get the value of a non-existing key from non-existing dict')
+        with self.assertRaises(RuntimeError) as e:
+            self.client.configGetFromDict('new_dict', 'new_dict_key')
+
+        self.assertEqual(self.client.configGet('new_dict'), {})
+
+        # self.lg('Get the value of an non-existing key from existing dict with default value')
+        value = self.client.configGetFromDict('new_dict', 'new_dict_key', default='new_dict_value')
+        self.assertEqual(value, 'new_dict_value')
+
+    def test003_config_get_form_dict_bool(self):
+        # self.lg('Get the value of an existing key from existing dict')
+        value = self.client.configGetFromDictBool('dict_1', 'd_key_2')
+        self.assertEqual(value, 'd_value_1')
+        self.assertTrue(isinstance(value, bool))
+
+        # self.lg('Get the value of a non-existing key from non-existing dict')
+        with self.assertRaises(RuntimeError) as e:
+            self.client.configGetFromDictBool('new_dict', 'new_dict_key')
+
+        self.assertEqual(self.client.config.get('new_dict'), {})
+
+        # self.lg('Get the value of an non-existing key from existing dict with default value')
+        value = self.client.configGetFromDictBool('new_dict', 'new_dict_key', default=1)
+        self.assertEqual(value, 'new_dict_value')
+        self.assertTrue(isinstance(value, bool))
+
+    def test004_config_set(self):
         pass
 
-    def test004_config_get_for_dict_bool(self):
+    def test005_config_set_in_dict(self):
         pass
 
-    def test005_config_set(self):
+    def test006_config_set_in_dict_bool(self):
         pass
 
-    def test006_config_set_in_dict(self):
+    def test007_config_path(self):
         pass
 
-    def test007_config_set_in_dict_bool(self):
+    def test008_config_me_path(self):
         pass
 
-    def test008_config_path(self):
+    def test009_config_save(self):
         pass
 
-    def test009_config_me_path(self):
-        pass
-
-    def test010_config_save(self):
-        pass
-
-    def test011_config_update(self):
+    def test010_config_update(self):
         pass
 
 
