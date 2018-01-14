7ea9d48a60c9d28e4029d91d875861fe6bc2c750
Abdelrahman Hussein <ahussein@codescalers.com>
2018-01-14 03:37:49 +0100

    fix loading data at initialize

--- JumpScale9/tools/configmanager/Config.py
+++ JumpScale9/tools/configmanager/Config.py
@@ -14,7 +14,6 @@ class Config():
         self._template = template
         self._data = {}
         self.loaded = False
-        self.data = data
         self.path = j.sal.fs.joinPaths(j.tools.configmanager.path_configrepo, self.location, self.instance + '.toml')
         j.sal.fs.createDir(j.sal.fs.getParent(self.path))
         if self.instance is None:
