threefoldtech/core9

commit 7f603a6f7b448f0f65c92a1ed7c14cf85a907165

numtools, sheet

JumpScale9/clients/redis/RedisFactory.py
@@ -74,18 +74,24 @@ class RedisFactory:
         if that doesn't work then will look for std redis port
         if that does not work then will return None
         """
-        if "TMPDIR" in os.environ:
-            tmpdir = os.environ["TMPDIR"]
-        else:
-            tmpdir = "/tmp"
+        # if "TMPDIR" in os.environ:
+        #     tmpdir = os.environ["TMPDIR"]
+        # else:
+        #     tmpdir = "/tmp"
 
-        unix_socket_path = '%s/redis.sock' % tmpdir
+        # tmpdir= j.core.dirs.TMPDIR
+
+        unix_socket_path = '%s/redis.sock' % j.core.dirs.TMPDIR
 
         db = None
-        if os.path.exists(path=unix_socket_path) and j.sal.process.checkProcessRunning('redis-server'):
+        if os.path.exists(path=unix_socket_path):
             db = Redis(unix_socket_path=unix_socket_path)
-        elif self._tcpPortConnectionTest("localhost", 6379, timeout=None):
+        elif j.sal.nettools.tcpPortConnectionTest("localhost",6379):
             db = Redis()
+        else:
+            self.start4core()
+            db = Redis(unix_socket_path=unix_socket_path)
+
 
         # try:
         #     j.core.db.set("internal.last", 0)
@@ -95,20 +101,6 @@ class RedisFactory:
 
         return db
 
-    def _tcpPortConnectionTest(self, ipaddr, port, timeout=None):
-        conn = None
-        try:
-            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            if timeout:
-                conn.settimeout(timeout)
-            try:
-                conn.connect((ipaddr, port))
-            except BaseException:
-                return False
-        finally:
-            if conn:
-                conn.close()
-        return True
 
     def kill(self):
         j.sal.process.execute("redis-cli -s %s/redis.sock shutdown" %

JumpScale9/data/cache/Cache.py
@@ -10,6 +10,11 @@ class Cache:
         self.__jslocation__ = "j.data.cache"
         self._cache = {}
 
+    def redis_local_get(self,reset=False, expiration=30):
+        db=j.data.kvs.getRedisStore(namespace="cache")
+        return self.get(db=db,reset=reset,expiration=expiration)
+
+
     def get(self, id="main", db=None, reset=False, expiration=30):
         """
         @param id is a unique id for the cache

JumpScale9/data/key_value_store/StoreFactory.py
@@ -123,39 +123,26 @@ class StoreFactory:
 
         print("test ok")
 
-    def getRedisCacheLocal(self):
-        """
-        example:
-        cache=j.data.kvs.getRedisCacheLocal()
-        serializer=j.data.serializer.json
-        db=j.data.kvs.getRedisStore(namespace="cache",serializers=[serializer],cache=cache)
+    # def getRedisCacheLocal(self):
+    #     """
+    #     example:
+    #     cache=j.data.kvs.getRedisCacheLocal()
+    #     serializer=j.data.serializer.json
+    #     db=j.data.kvs.getRedisStore(namespace="cache",serializers=[serializer],cache=cache)
 
-        """
-        # for now just local to test
-
-        # the std redis is not on tcp, so this will not work in all cases, had to modify
-
-        if self._redisCacheLocal is None:
-            cache = self.getRedisStore(
-                name='kvs-cache',
-                namespace='cache',
-                host='localhost',
-                port=6379,
-                db=0,
-                password='',
-                serializers=[]
-            )
-            try:
-                cache.set("test", "test")
-            except Exception as e:
-                cache = self.getRedisStore('kvs-cache', namespace='cache', host='localhost',
-                                           unixsocket='%s/redis.sock' % j.dirs.TMPDIR,
-                                           db=0, password='',
-                                           serializers=[])
-                cache.set("test", "test")
-            self._redisCacheLocal = cache
-
-        return self._redisCacheLocal
+    #     """
+        
+    #     if self._redisCacheLocal is None:
+    #         cache = self.getRedisStore(
+    #             name='kvs-cache',
+    #             redisclient = None,
+    #             namespace='cache',
+    #             password='',
+    #             serializers=[]
+    #         )
+    #         cache.set("test", "test")
+    #         self._redisCacheLocal = cache
+    #     return self._redisCacheLocal
 
     # def getFSStore(self, name, namespace='', baseDir=None, serializers=[], cache=None, changelog=None, masterdb=None):
     #     '''
@@ -203,11 +190,8 @@ class StoreFactory:
     def getRedisStore(
             self,
             name="core",
+            redisclient=None,
             namespace='db',
-            host='localhost',
-            port=None,
-            unixsocket=None,
-            db=0,
             password='',
             serializers=None,
             masterdb=None,
@@ -225,28 +209,15 @@ class StoreFactory:
         @return: key value store
         @rtype: MemoryKeyValueStore
         '''
-        if not port:
-            port = 6379
-            if j.core.state.configGetFromDict("redis", "port", ""):
-                port = j.core.state.configGetFromDict("redis", "port")
-        if not unixsocket:
-            unixsocket = None
-            if j.core.state.configGetFromDict("redis", "unixsocket", ""):
-                port = j.core.state.configGetFromDict("redis", "unixsocket")
-
         from JumpScale9.data.key_value_store.redis_store import RedisKeyValueStore
 
-        if j.clients.redis.get4core() is None:
-            j.clients.redis.start4core()
+        if redisclient==None:
+            redisclient=j.clients.redis.get4core()
 
         res = RedisKeyValueStore(
             name=name,
+            redisclient=redisclient,
             namespace=namespace,
-            host=host,
-            port=port,
-            unixsocket=unixsocket,
-            db=db,
-            password=password,
             serializers=serializers,
             masterdb=masterdb,
             changelog=changelog,

JumpScale9/data/key_value_store/redis_store.py
@@ -16,20 +16,15 @@ class RedisKeyValueStore(KeyValueStoreBase):
     def __init__(
             self,
             name,
-            namespace="db",
-            host='localhost',
-            port=6379,
-            unixsocket=None,
-            db=0,
-            password='',
+            redisclient,
+            namespace="db",            
             serializers=[],
             masterdb=None,
             cache=None,
             changelog=None):
 
-        self.redisclient = j.clients.redis.get(host, port, password=password, unixsocket=unixsocket)
-        # self.redisclient.port = port
-        # self.redisclient.host = host
+        self.redisclient = redisclient
+
         KeyValueStoreBase.__init__(self, namespace=namespace, name=name, serializers=serializers,
                                    masterdb=masterdb, cache=cache, changelog=changelog)
         self._indexkey = "index:%s" % namespace

JumpScale9/data/numtools/NumTools.py
@@ -7,7 +7,11 @@ class NumTools:
     def __init__(self):
         self.__jslocation__ = "j.tools.numtools"
         self.__imports__ = "numpy"
-        self.currencies = {}
+        self._currencies = {}
+
+    @property
+    def currencies(self):
+        return j.clients.currencylayer.cur2usd
 
     def _getYearFromMonthId(self, monthid, startyear=0):
         """
@@ -135,15 +139,6 @@ class NumTools:
                 result.append(item)
         return result
 
-    def _initCurrencies(self):
-        if self.currencies == {}:
-            path = j.sal.fs.joinPaths("cfg", "currencies.cfg")
-            if j.sal.fs.exists(path):
-                ini = j.tools.inifile.open(path)
-                if ini.checkSection("eur"):
-                    for cur in list(ini.getSectionAsDict("eur").keys()):
-                        self.currencies[cur] = ini.getFloatValue("eur", cur)
-
     def text2val(self, value):
         """
         value can be 10%,0.1,100,1m,1k  m=million
@@ -153,16 +148,22 @@ class NumTools:
         e.g.: 10EUR or 10 EUR (spaces are stripped)
         e.g.: 0.1mEUR or 0.1m EUR or 100k EUR or 100000 EUR
         """
-        if not j.data.types.string(value):
+        if not j.data.types.string.check(value):
             raise j.exceptions.RuntimeError("value needs to be string in text2val, here: %s" % value)
-        if value.lower().find("eur") != -1:
-            value = value.replace("eur", "").strip()
-        self._initCurrencies()
+        
         cur = 1.0
-        for cur2 in list(self.currencies.keys()):
-            if value.find(cur2) != -1:
-                value = value.replace(cur2, "").strip()
-                cur = self.currencies[cur2]
+        try:
+            #dirty trick to see if value can be float, if not will look for currencies
+            float(value)
+        except Exception as e:
+            value=value.lower()
+            for cur2 in list(self.currencies.keys()):
+                # print(cur2)
+                if value.find(cur2) != -1:
+                    # print("FOUND")
+                    value = value.lower().replace(cur2, "").strip()
+                    cur = 1/self.currencies[cur2]
+
         if value.find("k") != -1:
             value = float(value.replace("k", "").strip()) * 1000
         elif value.find("m") != -1:
@@ -171,3 +172,13 @@ class NumTools:
             value = float(value.replace("%", "").strip()) / 100
         value = float(value) * cur
         return value
+
+    def test(self):
+        """
+        js9 'j.tools.numtools.test()'
+        """
+        assert  self.text2val("10k")==10000.0
+
+        assert self.currencies["egp"]*10000000 == self.text2val("10 m egp")
+        assert self.currencies["egp"]*10000000 == self.text2val("10m egp")
+        assert self.currencies["egp"]*10000000 == self.text2val("10mEGP")

JumpScale9/data/serializers/SerializerDict.py
@@ -1,6 +1,7 @@
 from JumpScale9 import j
 import copy
 
+
 class SerializerDict:
 
     def _unique_list(self, items, sort=False, strip=False):
@@ -22,7 +23,7 @@ class SerializerDict:
 
         @return dict,errors=[]
 
-        in errors there will be key's which are not found in dictsource
+        in errors is list of list [[key,val],...]
 
         """
         if key not in dictsource.keys():
@@ -31,22 +32,28 @@ class SerializerDict:
                 return dictsource, errors
             else:
                 if die:
-                    raise j.exceptions.Input("dictsource does not have key:%s, can insert value" % key)
+                    raise j.exceptions.Input(
+                        "dictsource does not have key:%s, can insert value" % key)
                 else:
-                    errors.append(key)
+                    errors.append((key, val))
+                    return dictsource, errors
 
         if j.data.types.list.check(dictsource[key]):
             # check is list & set the types accordingly
             if j.data.types.string.check(val):
                 if "," in val:
-                    val = [item.replace("'", "").strip() for item in val.split(",")]
+                    val = [item.replace("'", "").strip()
+                           for item in val.split(",")]
                 else:
                     val = [val]
             elif j.data.types.int.check(val) or j.data.types.float.check(val):
                 val = [val]
 
             if listunique:
-                dictsource[key] = self._unique_list(val, sort=listsort, strip=liststrip)
+                dictsource[key] = self._unique_list(
+                    val, sort=listsort, strip=liststrip)
+            else:
+                dictsource[key] = val
 
         elif j.data.types.bool.check(dictsource[key]):
             if str(val).lower() in ['true', "1", "y", "yes"]:
@@ -79,13 +86,14 @@ class SerializerDict:
         @return dictsource,errors
 
         """
-        if not j.data.types.dict.check(dictsource) or not j.data.types.dict.check(dictupdate) :
-            raise j.exceptions.Input("dictsource and dictupdate need to be dicts")
+        if not j.data.types.dict.check(dictsource) or not j.data.types.dict.check(dictupdate):
+            raise j.exceptions.Input(
+                "dictsource and dictupdate need to be dicts")
 
         keys = [item for item in dictupdate.keys()]
         keys.sort()
 
-        dictsource = copy.copy(dictsource) #otherwise template gets changed
+        dictsource = copy.copy(dictsource)  # otherwise template gets changed
 
         for key in keys:
             val = dictupdate[key]

JumpScale9/data/serializers/SerializerTOML.py
@@ -4,7 +4,7 @@ import pytoml
 
 from .SerializerBase import SerializerBase
 
-testtemplate="""
+testtemplate = """
 name = ''
 
 multiline = ''
@@ -27,7 +27,7 @@ list5 = [ ]
 
 """
 
-testtoml="""
+testtoml = """
 name = 'something'
 
 multiline = '''
@@ -54,10 +54,9 @@ list5 = "d,a,a,b,c"
 """
 
 
-
 class SerializerTOML(SerializerBase):
 
-    def fancydumps(self,obj,secure=False):
+    def fancydumps(self, obj, secure=False):
         """
         if secure then will look for key's ending with _ and will use your secret key to encrypt (see nacl client)
         """
@@ -65,59 +64,66 @@ class SerializerTOML(SerializerBase):
         if not j.data.types.dict.check(obj):
             raise j.exceptions.Input("need to be dict")
 
-        keys=[item for item in obj.keys()]
+        keys = [item for item in obj.keys()]
         keys.sort()
 
-        out=""
-        prefix=""
-        lastprefix=""
+        out = ""
+        prefix = ""
+        lastprefix = ""
         for key in keys:
 
-            val=obj[key]
+            val = obj[key]
 
-            #get some vertical spaces between groups which are not equal
+            # get some vertical spaces between groups which are not equal
             if "." in key:
-                prefix,key.split(".",1)
-            elif "_" in key:
-                prefix,key.split("_",1)
+                prefix, key.split(".", 1)
+            # elif "_" in key:
+            #     prefix, key.split("_", 1)
             else:
-                prefix=key[0:2]
-            if prefix!=lastprefix:
-                out+="\n"
-                lastprefix=prefix
+                prefix = key[0:6]
 
-            ttype=j.data.types.type_detect(val)
+            if prefix != lastprefix:
+                out += "\n"
+                # print("PREFIXCHANGE:%s:%s" % (prefix, lastprefix))
+                lastprefix = prefix
+            # else:
+                # print("PREFIXNOCHANGE:%s:%s" % (prefix, lastprefix))
+
+            ttype = j.data.types.type_detect(val)
             if secure and key.endswith("_") and ttype.BASETYPE == "string":
-                val = j.data.nacl.default.encryptSymmetric(val,hex=True,salt=val)
+                val = j.data.nacl.default.encryptSymmetric(
+                    val, hex=True, salt=val)
 
-            out+="%s\n"%(ttype.toml_string_get(val,key=key))
+            out += "%s\n" % (ttype.toml_string_get(val, key=key))
 
             # else:
             #     raise RuntimeError("error in fancydumps for %s in %s"%(key,obj))
 
-        return j.data.text.strip(out)
+        out = out.replace("\n\n\n", "\n\n")
 
+        return j.data.text.strip(out)
 
     def dumps(self, obj):
         return pytoml.dumps(obj, sort_keys=True)
 
-    def loads(self, s,secure=False):
+    def loads(self, s, secure=False):
         if isinstance(s, bytes):
             s = s.decode('utf-8')
         try:
             val = pytoml.loads(s)
         except Exception as e:
-            raise RuntimeError("Toml deserialization failed for:\n%s.\nMsg:%s"%(j.data.text.indent(s),str(e)))
+            raise RuntimeError("Toml deserialization failed for:\n%s.\nMsg:%s" % (
+                j.data.text.indent(s), str(e)))
         if secure and j.data.types.dict.check(val):
-            res={}
-            for key,item in val.items():
+            res = {}
+            for key, item in val.items():
                 if key.endswith("_"):
-                    res[key]=j.data.nacl.default.decryptSymmetric(item,hex=True).decode()
-            val=res
+                    res[key] = j.data.nacl.default.decryptSymmetric(
+                        item, hex=True).decode()
+            val = res
         return val
 
-
-    def merge(self,tomlsource, tomlupdate,keys_replace={},add_non_exist=False,die=True,errors=[],listunique=False,listsort=True,liststrip=True):
+    def merge(self, tomlsource, tomlupdate, keys_replace={}, add_non_exist=False, die=True, errors=[], listunique=False, listsort=True, liststrip=True):
         """
         the values of the tomlupdate will be applied on tomlsource (are strings or dicts)
 
@@ -128,106 +134,144 @@ class SerializerTOML(SerializerBase):
         listsort means that items in list will be sorted (list at level 1 under dict)
 
         @return dict,errors
-        
+
         """
         if j.data.types.string.check(tomlsource):
             try:
                 dictsource = self.loads(tomlsource)
             except Exception:
-                raise RuntimeError("toml file source is not properly formatted.")
+                raise RuntimeError(
+                    "toml file source is not properly formatted.")
         else:
             dictsource = tomlsource
         if j.data.types.string.check(tomlupdate):
             try:
                 dictupdate = self.loads(tomlupdate)
             except Exception:
-                raise RuntimeError("toml file source is not properly formatted.")
+                raise RuntimeError(
+                    "toml file source is not properly formatted.")
         else:
             dictupdate = tomlupdate
-        
-        return j.data.serializer.dict.merge(dictsource, dictupdate,keys_replace=keys_replace,add_non_exist=add_non_exist,die=die,\
-                errors=errors,listunique=listunique,listsort=listsort,liststrip=liststrip)
-        
+
+        return j.data.serializer.dict.merge(dictsource, dictupdate, keys_replace=keys_replace, add_non_exist=add_non_exist, die=die,
+                                            errors=errors, listunique=listunique, listsort=listsort, liststrip=liststrip)
 
     def test(self):
+        """
+        js9 'j.data.serializer.toml.test()'
+        """
 
-        ddict=self.loads(testtoml)
-        template=self.loads(testtemplate)
+        ddict = self.loads(testtoml)
+        template = self.loads(testtemplate)
 
-        ddictout,errors=self.merge(template,ddict,listunique=True)
+        ddictout, errors = self.merge(template, ddict, listunique=True)
 
-        ddicttest={'name': 'something', 'multiline': 'these are multiple lines\nnext line\n', 'nr': 87, 'nr2': 0, 'nr3': 1, 'nr4': 34.4, 'nr5': 34.4, 'bbool': True, 'bbool2': True, 'bbool3': False, 'list1': ['1', '2', '3', '4'], 'list2': [1, 2, 3], 'list3': ['a', 'b', 'c'], 'list4': ['ab'], 'list5': ['a', 'b', 'c', 'd']}
+        ddicttest = {'name': 'something', 'multiline': 'these are multiple lines\nnext line\n', 'nr': 87, 'nr2': 0, 'nr3': 1, 'nr4': 34.4, 'nr5': 34.4, 'bbool': True,
+                     'bbool2': True, 'bbool3': False, 'list1': ['1', '2', '3', '4'], 'list2': [1, 2, 3], 'list3': ['a', 'b', 'c'], 'list4': ['ab'], 'list5': ['a', 'b', 'c', 'd']}
 
         print(ddictout)
 
-        assert ddictout==ddicttest
+        assert ddictout == ddicttest
 
-        ddictmerge={'nr': 88}
+        ddictmerge = {'nr': 88}
 
-        #start from previous one, update
-        ddictout,errors=self.merge(ddicttest,ddictmerge,listunique=True)
+        # start from previous one, update
+        ddictout, errors = self.merge(ddicttest, ddictmerge, listunique=True)
 
-        ddicttest={'name': 'something', 'multiline': 'these are multiple lines\nnext line\n', 'nr': 88, 'nr2': 0, 'nr3': 1, 'nr4': 34.4, 'nr5': 34.4, 'bbool': True, 'bbool2': True, 'bbool3': False, 'list1': ['1', '2', '3', '4'], 'list2': [1, 2, 3], 'list3': ['a', 'b', 'c'], 'list4': ['ab'], 'list5': ['a', 'b', 'c', 'd']}
+        ddicttest = {'name': 'something', 'multiline': 'these are multiple lines\nnext line\n', 'nr': 88, 'nr2': 0, 'nr3': 1, 'nr4': 34.4, 'nr5': 34.4, 'bbool': True,
+                     'bbool2': True, 'bbool3': False, 'list1': ['1', '2', '3', '4'], 'list2': [1, 2, 3], 'list3': ['a', 'b', 'c'], 'list4': ['ab'], 'list5': ['a', 'b', 'c', 'd']}
 
-        assert ddictout==ddicttest
+        assert ddictout == ddicttest
 
-        ddictmerge={'nr_nonexist': 88}
+        ddictmerge = {'nr_nonexist': 88}
 
-        #needs to throw error
+        # needs to throw error
         try:
-            error=0
-            ddictout,errors=self.merge(ddicttest,ddictmerge,listunique=True)
+            error = 0
+            ddictout, errors = self.merge(
+                ddicttest, ddictmerge, listunique=True)
         except:
-            error=1
+            error = 1
         assert 1
 
-        ddictmerge={}
-        ddictmerge["list1"]=[]
+        ddictmerge = {}
+        ddictmerge["list1"] = []
         for i in range(20):
-            ddictmerge["list1"].append("this is a test %s"%i)
-        ddictout,errors=self.merge(ddicttest,ddictmerge,listunique=True)
+            ddictmerge["list1"].append("this is a test %s" % i)
+        ddictout, errors = self.merge(ddicttest, ddictmerge, listunique=True)
 
+        yyaml = self.fancydumps(ddictout)
+        print(yyaml)
 
-        yyaml=self.fancydumps(ddictout)
-        print (yyaml)
+        compare = {'bbool': True,
+                   'bbool2': True,
+                   'bbool3': False,
+                   'list1': ['this is a test 0',
+                             'this is a test 1',
+                             'this is a test 10',
+                             'this is a test 11',
+                             'this is a test 12',
+                             'this is a test 13',
+                             'this is a test 14',
+                             'this is a test 15',
+                             'this is a test 16',
+                             'this is a test 17',
+                             'this is a test 18',
+                             'this is a test 19',
+                             'this is a test 2',
+                             'this is a test 3',
+                             'this is a test 4',
+                             'this is a test 5',
+                             'this is a test 6',
+                             'this is a test 7',
+                             'this is a test 8',
+                             'this is a test 9'],
+                   'list2': [1, 2, 3],
+                   'list3': ['a', 'b', 'c'],
+                   'list4': ['ab'],
+                   'list5': ['a', 'b', 'c', 'd'],
+                   'multiline': '    these are multiple lines\n    next line\n    ',
+                   'name': 'something',
+                   'nr': 88,
+                   'nr2': 0,
+                   'nr3': 1,
+                   'nr4': 34.4,
+                   'nr5': 34.4}
 
-        compare={'bbool': True,
-            'bbool2': True,
-            'bbool3': False,
-            'list1': ['this is a test 0',
-            'this is a test 1',
-            'this is a test 10',
-            'this is a test 11',
-            'this is a test 12',
-            'this is a test 13',
-            'this is a test 14',
-            'this is a test 15',
-            'this is a test 16',
-            'this is a test 17',
-            'this is a test 18',
-            'this is a test 19',
-            'this is a test 2',
-            'this is a test 3',
-            'this is a test 4',
-            'this is a test 5',
-            'this is a test 6',
-            'this is a test 7',
-            'this is a test 8',
-            'this is a test 9'],
-            'list2': [1, 2, 3],
-            'list3': ['a', 'b', 'c'],
-            'list4': ['ab'],
-            'list5': ['a', 'b', 'c', 'd'],
-            'multiline': '    these are multiple lines\n    next line\n    ',
-            'name': 'something',
-            'nr': 88,
-            'nr2': 0,
-            'nr3': 1,
-            'nr4': 34.4,
-            'nr5': 34.4}
+        res = self.loads(yyaml)
 
-        res=self.loads(yyaml)
+        assert res == compare
 
-        assert res==compare
+        template = {'login': '', 'first_name': '', 'last_name': '', 'locations': [], 'companies': [], 'departments': [], 'languageCode': 'en-us', 'title': [], 'description_internal': '', 'description_public_friendly': '',
+                    'description_public_formal': '', 'experience': '', 'hobbies': '', 'pub_ssh_key': '', 'skype': '', 'telegram': '', 'itsyou_online': '', 'reports_into': '', 'mobile': [], 'email': [], 'github': '', 'linkedin': '', 'links': []}
+        toupdate = {'companies': ['threefold'],
+                    'company_id': [2],
+                    'departments': ['threefold:engineering', 'threefold:varia'],
+                    'description_internal': 'Researcher who develops new ideas for Threefold and creates concise explanations of difficult concepts',
+                    'description_public_formal': 'Develops new ideas for Threefold and creates concise explanations of difficult concepts.',
+                    'description_public_friendly': 'Virgil is a researcher and innovator who is always looking to improve the world around him both on a macro and micro scale.\n\nFor the past 11 years he has been working with new technologies, helping organizations integrate them into their existing services and create their new products.  \nHe holds a PhD in autonomous robotics, artificial intelligence and reliability.\n\nVirgil also lectures at a technical university and an academy.\n\n',
+                    'email': ['ilian.virgil@gmail.com', 'ilian@greenitglobe.com'],
+                    'name': 'virgil',
+                    'github': 'Virgil3',
+                    'hobbies': 'generative coding, movies, diving, languages',
+                    'itsyou_online': 'ilian@greenitglobe.com',
+                    'languageCode': 'en-us',
+                    'last_name': 'ilian',
+                    'linkedin': 'https://www.linkedin.com/in/ilian-virgil-342b8471',
+                    'links': [],
+                    'locations': ['bucharest'],
+                    'login': '',
+                    'mobile': ['+40721543908'],
+                    'pub_ssh_key': '',
+                    'reports_into': 'Kristof',
+                    'skype': 'ilian.virgil',
+                    'telegram': '@virgil_ilian',
+                    'title': ['Researcher']}
 
+        result, errors = self.merge(template, toupdate,  keys_replace={
+            'name': 'first_name'}, add_non_exist=False, die=False, errors=[])
 
+        assert [('company_id', [2])] == errors
+        assert 'bucharest' in result["locations"]
+        assert 'ilian.virgil@gmail.com' in result["email"]
+        assert 'company_id' not in result  # should not be in

commit 2cc9a359d193abc4fecc088480a170e60489fac1

Fix js loading
at this point we're still loading jumpscale, we can't use
j.sal.nettools just yet.

bug introduced in :
https://github.com/Jumpscale/core9/commit/7f603a6f7b448f0f65c92a1ed7c14cf85a907165

JumpScale9/clients/redis/RedisFactory.py
@@ -1,13 +1,13 @@
 from JumpScale9 import j
 
-redisFound=False
+redisFound = False
 try:
     from .Redis import Redis
     from .RedisQueue import RedisQueue
     from redis._compat import nativestr
     # import itertools
     import socket
-    redisFound=True
+    redisFound = True
 except:
     pass
 import os
@@ -22,7 +22,7 @@ class RedisFactory:
 
     def __init__(self):
         self.clearCache()
-        self.redisFound=redisFound
+        self.redisFound = redisFound
         self.__jslocation__ = "j.clients.redis"
 
     def clearCache(self):
@@ -39,7 +39,7 @@ class RedisFactory:
             unixsocket=None,
             ardb_patch=False,
             **args):
-        if redisFound==False:
+        if redisFound == False:
             raise RuntimeError("redis libraries are not installed, please pip3 install them.")
         if unixsocket is None:
             key = "%s_%s" % (ipaddr, port)
@@ -74,33 +74,35 @@ class RedisFactory:
         if that doesn't work then will look for std redis port
         if that does not work then will return None
         """
-        # if "TMPDIR" in os.environ:
-        #     tmpdir = os.environ["TMPDIR"]
-        # else:
-        #     tmpdir = "/tmp"
 
-        # tmpdir= j.core.dirs.TMPDIR
+        # can't use j.sal.nettools here, not loaded yet
+        def _tcpPortConnectionTest(ipaddr, port, timeout=None):
+            conn = None
+            try:
+                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                if timeout:
+                    conn.settimeout(timeout)
+                try:
+                    conn.connect((ipaddr, port))
+                except BaseException:
+                    return False
+            finally:
+                if conn:
+                    conn.close()
+            return True
 
         unix_socket_path = '%s/redis.sock' % j.core.dirs.TMPDIR
 
         db = None
         if os.path.exists(path=unix_socket_path):
             db = Redis(unix_socket_path=unix_socket_path)
-        elif j.sal.nettools.tcpPortConnectionTest("localhost",6379):
+        elif _tcpPortConnectionTest("localhost", 6379):
             db = Redis()
         else:
             self.start4core()
             db = Redis(unix_socket_path=unix_socket_path)
 
-
-        # try:
-        #     j.core.db.set("internal.last", 0)
-        #     return True
-        # except:
-        #     db = None
-
         return db
-
 
     def kill(self):
         j.sal.process.execute("redis-cli -s %s/redis.sock shutdown" %