32fe46e29b729960c3e4e6f643b9651cfbc04023
Abdelrahman Hussein <ahussein@codescalers.com>
2018-03-19 19:15:05 +0100

    add integration tests

--- test.sh
+++ test.sh
@@ -5,3 +5,9 @@ set -x
 export JS9MODE=TESTING
 pytest -v /opt/code/github/jumpscale/core9/tests/unittests/
 unset JS9MODE
+
+# run integration tests
+# init config manager
+python3 tests/basic.py
+
+pytest -v tests/integration_tests
