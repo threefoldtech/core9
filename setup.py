6ee030d24078183047eac58c748efedaf81d8adb
Reem Khamis <khamisr@codescalers.com>
2018-06-12 12:08:39 +0200

    bump version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ except ImportError:
 
 setup(
     name='JumpScale9',
-    version='9.4.0-rc2',
+    version='9.4.0-rc3',
     description='Automation framework for cloud workloads',
     long_description=long_description,
     url='https://github.com/Jumpscale/core9',
