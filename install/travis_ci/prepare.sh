ad723151ff50fdc079074a508e0622e64edac893
Abdelrahman Hussein <ahussein.abdelrahman@gmail.com>
2018-07-09 14:22:56 +0200

    update prepare script

--- install/travis_ci/prepare.sh
+++ install/travis_ci/prepare.sh
@@ -4,10 +4,15 @@ set -e
 sudo ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
 export SSHKEYNAME=id_rsa
 
-export ZUTILSBRANCH=${ZUTILSBRANCH:-master}
+export ZUTILSBRANCH=${ZUTILSBRANCH:-development}
+export JS9BRANCH=development
 
 curl https://raw.githubusercontent.com/Jumpscale/bash/$ZUTILSBRANCH/install.sh?$RANDOM > /tmp/install.sh;sudo -E bash /tmp/install.sh
 
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZCodeGetJS"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerInstallLocal"
-sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZInstall_js9_full"
+. ~/.bashrc
+
+ZInstall_host_js
+
+# sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZCodeGetJS"
+# sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZDockerInstallLocal"
+# sudo -HE bash -c "source /opt/code/github/jumpscale/bash/zlibs.sh; ZKeysLoad; ZInstall_js9_full"
