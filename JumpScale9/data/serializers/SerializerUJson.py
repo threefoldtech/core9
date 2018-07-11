9038a4821688448830c1ba6a132689ec54380354
hamdy <hamdy.a.farag@gmail.com>
2018-07-11 18:00:37 +0200

    added json encoder to properly encode bytes

--- JumpScale9/data/serializers/SerializerUJson.py
+++ JumpScale9/data/serializers/SerializerUJson.py
@@ -7,13 +7,20 @@ except BaseException:
 from .SerializerBase import SerializerBase
 
 
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        return json.JSONEncoder.default(self, obj)
+
+
 class SerializerUJson(SerializerBase):
 
     def __init__(self):
         SerializerBase.__init__(self)
 
     def dumps(self, obj, sort_keys=False, indent=False):
-        return json.dumps(obj, ensure_ascii=False, sort_keys=sort_keys, indent=indent)
+        return json.dumps(obj, ensure_ascii=False, sort_keys=sort_keys, indent=indent, cls=Encoder)
 
     def loads(self, s):
         if isinstance(s, bytes):
