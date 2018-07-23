5b7f058d5d00f9d2fe29898a746c372c7f63ea62
BolaNasr <bola.e.nasr@gmail.com>
2018-07-23 15:32:30 +0200

    replace logger.info to debug to get information only when debug mode is enable #243

--- JumpScale9/clients/ssh/SSHClient.py
+++ JumpScale9/clients/ssh/SSHClient.py
@@ -62,11 +62,9 @@ class SSHClient(SSHClientBase):
                     printer(line)
             return buffer
 
-        out = _consume_stream(stdout, self.logger.info)
+        out = _consume_stream(stdout, self.logger.debug)
         err = _consume_stream(stderr, self.logger.error)
         self._client.wait_finished(channel)
-        _consume_stream(stdout, self.logger.info, out)
-        _consume_stream(stderr, self.logger.error, err)
 
         rc = channel.get_exit_status()
         output = out.getvalue()
