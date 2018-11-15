5e21d310c1aeb321625d1e77827d3be796cc5ee8
Maxime Daniel <root@maxux.net>
2018-11-15 15:12:09 +0100

    Stick to redis < 3.0

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         'python-dateutil>=2.6.0',
         'pytoml>=0.1.12',
         'toml',
-        'redis>=2.10.5',
+        'redis<3.0.0',
         'requests>=2.13.0',
         'future>=0.16.0',
         'watchdog',
